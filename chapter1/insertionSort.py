0fd24fd7167ee6a3a5bce33a40dc0d3891dcfcbb
Relativiteit <alejocain@gmail.com>
2021-08-29 23:42:28 +0200

    addded python code

--- chapter1/insertionSort.py
+++ chapter1/insertionSort.py
@@ -9,3 +9,18 @@ for j = 2 to A.length
         i = i - 1
     A[i+1] = key """ 
 
+def insertion_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1 
+        # insert A[j] into the sorted sequence A[1...j-1]
+        while i>=0 and key < A[i]:
+            A[i+1] = A[i]
+            i = i -1 
+            A[i+1] = key
+
+if __name__ == '__main__':
+    A = [39,12,40,5,8,9]
+    insertion_sort(A)
+    print(A)
+
